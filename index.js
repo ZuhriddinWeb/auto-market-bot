7854963294163548b183b18dc265dca7b8c71dd2
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2026-08-25 16:43:36 +0500

    changed comment

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ const mainMenu = new Keyboard()
   .text("📝 E'lon berish").text("🔍 Mashina qidirish").row()
   .text("📂 Mening e'lonlarim").text("🎁 Bepul VIP (UP)").row()
   .text("🧮 Mashina narxini aniqlash").row().text("🔔 Obunalarim").row()
-  .text("🏆 150.000 so'm Yutib oling!").resized()
+  // .text("🏆 150.000 so'm Yutib oling!").resized()
   .placeholder("Tugmalarni ochish uchun shu yerni bosing 🎛🎛👉");
 /**
  * ✅ МАЖБУРИЙ ОБУНАНИ ТЕКШИРУВЧИ ФУНКЦИЯЛАР
